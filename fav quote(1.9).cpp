7e996ba46ffac6bf3e6e88923e7dd1fb52dbb560
Rajanya-glitch <111448418+Rajanya-glitch@users.noreply.github.com>
2023-09-15 14:44:18 +0530

    Update fav quote(1.9).cpp
    this is for demo

--- fav quote(1.9).cpp
+++ fav quote(1.9).cpp
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main()
 {
-	printf("The light and the dark moments come together and create a beautiful artwork,the artwork is life:)";
+	printf("The light and the dark moments come together and create a beautiful artwork,the artwork is life");
 	
 	
 }
