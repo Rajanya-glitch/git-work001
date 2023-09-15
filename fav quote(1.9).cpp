f8885ec6ad29e3f7d6871ddb99ac3eae4331ae22
Rajanya-glitch <rajanyaroybarman125@gmail.com>
2023-09-15 13:39:20 +0530

    new file added.

---
+++ fav quote(1.9).cpp
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main()
+{
+	printf("The Best way to predict the future is to create it:)\nThe light and the dark moments come together and create a beautiful artwork,the artwork is life:)");
+	
+	
+}
